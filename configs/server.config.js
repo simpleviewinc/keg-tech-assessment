f9a86d2343d49d452206d777e4e08bfd3faa081d
Joshua Williams <joshuawilliamsguitar@gmail.com>
2024-06-10 08:45:02 -0700

    Add docker file

--- configs/server.config.js
+++ configs/server.config.js
@@ -2,7 +2,7 @@ const {uuid} = require('@keg-hub/jsutils')
 
 module.exports = {
   api: {
-    origins: ['http://localhost:3000', '0.0.0.0:3000'],
+    origins: ['http://localhost:3000', 'http://0.0.0.0:3000'],
     port: 5005,
     host: '0.0.0.0',
     uuid: uuid(),
