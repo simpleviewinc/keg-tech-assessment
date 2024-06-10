270da75aecb1228a5234191d4d54d3119da4a8f0
eric truong <eric.truong@simpleviewinc.com>
2024-06-09 23:11:45 -0700

    update origin to have 'localhost:3000'

--- configs/server.config.js
+++ configs/server.config.js
@@ -2,8 +2,8 @@ const { uuid } = require('@keg-hub/jsutils')
 
 module.exports = {
   api: {
-    /**I update origins to use ['*'] to able to access from localhost:3000, or other port (not secure way) */
-    origins: ['*'],
+    /**I update origins to able to access from localhost:3000, or we can use '*' for any other port but not good practice */
+    origins: ['localhost:3000'],
     port: 5005,
     host: '0.0.0.0',
     uuid: uuid(),
