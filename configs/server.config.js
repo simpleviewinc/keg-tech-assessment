719d39185ee305161fd8fc119fb09d979af7fdbd
eric truong <eric.truong@simpleviewinc.com>
2024-06-09 21:33:45 -0700

    update

--- configs/server.config.js
+++ configs/server.config.js
@@ -2,8 +2,8 @@ const { uuid } = require('@keg-hub/jsutils')
 
 module.exports = {
   api: {
-    /**I update origins to included localhost:3000 so we can access the port to connect goats at the endpoints */
-    origins: ['http://localhost:3000'],
+    /**I update origins to use ['*'] to able to access from localhost:3000, or other port (not secure way) */
+    origins: ['*'],
     port: 5005,
     host: '0.0.0.0',
     uuid: uuid(),
--- src/getGoatFacts.js
+++ src/getGoatFacts.js
@@ -1,8 +1,13 @@
 /**
  * getGoatFacts - Gets a list of goat facts from the backend API
  */
+
+//update calling apiURL from server.config instead of hardcode fetch('localhost:5005/goats')
+const { api: config } = require('../configs/server.config')
+const apiURl = `http://${config.host}:${config.port}`
+
 export const getGoatFacts = async () => {
-  const res = await fetch('http://localhost:5005/goats');
+  const res = await fetch(`${apiURl}/goats`);
 
   if(res.ok){
     const facts = await res.json()
